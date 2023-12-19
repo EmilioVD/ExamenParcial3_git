747170712886f4e0a8da52a1c3079c76f8d3bfbf
Varguez DuranJose Emilio <141971906+EmilioVD@users.noreply.github.com>
2023-12-19 08:20:55 -0600

    Añado la interfaz mascota y la clase mascota
    lo que realizo es la creacion de los apartados donde  estara la logica de la mascota

---
+++ Examen Parcial 3/Mascotas/Interface_mascota.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Parcial_3.Mascotas
+{
+    interface Interface_mascota
+    {
+    }
+}
