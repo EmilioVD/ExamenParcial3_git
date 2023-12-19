fd92a2d777c07ef5b1e0f8019775297f3f28673e
NaviMhe <003navi@gmail.com>
2023-12-19 09:35:01 -0600

    Implementacion de el menu principal
    aqui estoy llamando a futuras funciones que van a hacer funcionar el menu principal

--- Examen Parcial 3/Program.cs
+++ Examen Parcial 3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Examen_Parcial_3.Mascotas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,39 @@ namespace Examen_Parcial_3
         {
             Console.WriteLine("Inicio");
 
+            static List<Persona> personas = new List<Persona>();
+            static List<Mascota> mascotas = new List<Mascota>();
 
+            static void Main()
+            {
+                while (true)
+                {
+                    MostrarMenuPrincipal();
+                    int opcion = LeerEntero("Seleccione una opción: ");
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            AdministrarCentro();
+                            break;
+                        case 2:
+                            AdministrarAdopciones();
+                            break;
+                        case 3:
+                            AdministrarBienestarAnimal();
+                            break;
+                        case 4:
+                            SimularInteracciones();
+                            break;
+                        case 5:
+                            Console.WriteLine("Programa finalizado.");
+                            return;
+                        default:
+                            Console.WriteLine("Opción no válida. Por favor, seleccione una opción válida.");
+                            break;
+                    }
+                }
+            }
 
 
 
