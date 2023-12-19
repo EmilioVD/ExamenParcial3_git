3f03495b6fec9f28e594171f94959a00cf379f34
Varguez DuranJose Emilio <141971906+EmilioVD@users.noreply.github.com>
2023-12-19 08:36:02 -0600

    Ajunto las interfaces para mascota y de igual manera logia en la clase mascota
    linkeo las interfaces y enumeradores y realizo la logica que tendra la clase mascota

--- Examen Parcial 3/Mascotas/Class_mascota.cs
+++ Examen Parcial 3/Mascotas/Class_mascota.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Examen_Parcial_3.Mascotas;
 
 namespace Examen_Parcial_3.Mascotas
 {
     class Interfaz_mascota
     {
+        
     }
+    
+    public abstract class Mascota
+    {
+        private static int contadorId = 1;
+
+        public string Id { get; private set; }
+        public string Nombre { get; private set; }
+        public int Edad { get; set; }
+        public Temperamento Temperamento { get; private set; }
+        public Persona Dueño { get; private set; }
+
+        protected Mascota(string nombre, Especie especie, Temperamento temperamento)
+        {
+            Id = $"{especie} - {contadorId++}";
+            Nombre = nombre;
+            Temperamento = temperamento;
+        }
+
+        public abstract void HacerRuido();
+
+        public void CambiarDueño(Persona nuevoDueño)
+        {
+            if (Dueño != null)
+            {
+                Console.WriteLine($"{Nombre} ha cambiado su dueño a {nuevoDueño.Nombre}.");
+            }
+
+            Dueño = nuevoDueño;
+        }
+    }
+    
 }
